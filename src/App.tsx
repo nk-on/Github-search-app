49d6269d2dc353dd75a7518cbad64bc582d4a9f9
nk-on <johndoe@example.com>
2024-11-27 22:21:01 +0400

    Feat:Adding feature which lets users to switch between night and day modes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import ColorSwitcher from './color-switcher/mode-switcher'
 import './App.css'
 
 function App() {
 
   return (
     <>
+     <ColorSwitcher />
     </>
   )
 }
---
+++ src/color-switcher/mode-switcher.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import moon from '../../src/assets/moon.svg'
+import sun from '../../src/assets/sun.svg'
+export default function ModeSwitcher(){
+    const [mode,setMode] = useState<string>("Day")
+    return <div className="Color-switcher w-[90%] h-[30vh] flex justify-around items-center cursor-pointer	">
+        <h2 className={ mode === "Day" ? "text-[#222731]":"text-white"}>Devfinder</h2>
+        <div className='Icon' onClick={():void=>{
+            mode === "Day" ? setMode("Night") : setMode("Day")
+            mode === "Day" ? document.body.style.backgroundColor = '#141D2F':document.body.style.backgroundColor = '#F6F8FF';
+        }}>{mode === "Day" ? <img src={moon} />: <img src={sun} />}</div>
+    </div>
+}
