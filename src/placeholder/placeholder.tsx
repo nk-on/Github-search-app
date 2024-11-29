674b74d9d1ab46fbd8e84de36d1b791dd7393270
nk-on <johndoe@example.com>
2024-11-29 22:09:09 +0400

    Style:adjusting width and height to placeholder

--- src/placeholder/placeholder.tsx
+++ src/placeholder/placeholder.tsx
@@ -2,7 +2,7 @@ import { ColorSwitch } from '../style-switcher/style-switcher';
 export default function PlaceHolder({ mode }: { mode: string }) {
   return (
     <div
-      className={`w-[730px] h-[419px] rounded-[15px] ${ColorSwitch(
+      className={`w-[90%] h-[419px] md:w-[730px] rounded-[15px] ${ColorSwitch(
         mode,
         'bg-white',
         'bg-[#1E2A47]'
