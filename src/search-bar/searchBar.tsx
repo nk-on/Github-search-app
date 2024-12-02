01e3148128e91b449951bd3a0a90516bb9f46210
nk-on <johndoe@example.com>
2024-12-02 23:02:31 +0400

    Feat:fetching API when user clicks on search button

--- src/search-bar/searchBar.tsx
+++ src/search-bar/searchBar.tsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from 'react';
 import PlaceHolder from '../placeholder/placeholder';
 import { ColorSwitch } from '../style-switcher/style-switcher';
+// import 'dotenv/config'
 export default function SearchBar({ mode }: { mode: string }) {
-  console.log(mode);
+  const [searchOn,setSearchOn] = useState(false);
+  const [query,setQuery] = useState('');
+  async function fetchData():Promise<unknown | void>{
+    if(query.length){
+      const res = await fetch(`https://api.github.com/users/${query}`,{
+        headers:{
+          Authorization:`Bearer ${import.meta.env.VITE_REACT_APP_GITHUB_TOKEN}`,
+        }
+      });
+      const data = await res.json();
+      console.log(data)
+      return data
+    }else{
+      return;
+    }
+  }
+  useEffect(()=>{
+    fetchData();
+  },[searchOn])
   return (
     <>
       <div className="w-full flex justify-center">
@@ -14,8 +34,14 @@ export default function SearchBar({ mode }: { mode: string }) {
               'bg-white',
               'bg-[#1E2A47]'
             )}`}
+            onChange={(event)=>{
+              setQuery(event.target.value)
+            }}
           />
-          <button className="bg-[#0079FF] w-[106px] h-[50px] text-white font-bold	rounded-[12px]">
+          <button className="bg-[#0079FF] w-[106px] h-[50px] text-white font-bold	rounded-[12px]" onClick={(e)=>{
+            e.preventDefault();
+            setSearchOn(true);
+          }}>
             Search
           </button>
         </form>
